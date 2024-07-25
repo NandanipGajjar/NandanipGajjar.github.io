e3a174b4cfa9f394a48128e0f6fff0cf8fc250e2
NandaniPGajjar <npgajjar2812@gmail.com>
2024-07-25 16:13:38 +0530

    DEPLOY 1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a434
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "380600859"
+    serviceWorkerVersion: "2044584625"
   }
 });
